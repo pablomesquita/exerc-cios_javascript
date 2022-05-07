1170d0bd716e8d72a65af611b2e99fa8dadcc72c
pablomesquita <pablomesquita06@gmail.com>
2022-05-07 01:06:10 -0300

    exercício javascript

---
+++ loop_array/index.js
@@ -0,0 +1,34 @@
+var alunos = [
+    { nome: 'Joao', notas: [5, 7.5, 8, 8, 4.5, 9] },
+    { nome: 'Breno', notas: [10, 8.5, 10, 7.5, 9, 6.5] },
+    { nome: 'Pedro', notas: [5.5, 8.3, 9.5, 8, 9, 6.9] },
+    { nome: 'Fulano', notas: [5.9, 8.5, 4, 8, 9.5, 6] },
+    { nome: 'Angelino', notas: [0, 4.8, 9.2, 8.5, 4.9, 6.1] },
+    { nome: 'Jaja', notas: [10, 4.3, 6.8, 8.25, 9.4, 6] },
+    { nome: 'Frederico', notas: [3.5, 8, 10, 8.5, 9, 3.8] },
+    { nome: 'Jonatas', notas: [6, 8.5, 8, 6.5, 7, 10] },
+    { nome: 'Henrique', notas: [4, 8.2, 5.5, 8.5, 5.4, 6] }
+]
+
+let mediaDoAluno = alunos.map(({ nome, notas }) => {
+
+    let somaDasNotas = 0;
+    notas.forEach((nota) => somaDasNotas += nota)
+    let media =(somaDasNotas / notas.length).toFixed(1);
+
+    return { nome, notas, media }
+
+})
+console.log(mediaDoAluno)
+
+
+
+
+
+// const alunosComMedia = alunos.map(({nome, notas}) => {
+//     let somaDasNotas = 0
+//     notas.forEach((nota) => somaDasNotas += nota)
+//     const media = somaDasNotas / notas.length
+//     return {nome, notas, media }
+//   })
+// console.log(alunosComMedia)
